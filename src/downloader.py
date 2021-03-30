1fa5c6e991a9554bcfa4510e9dbf42fffbaff401
Trenton W. Ford - Desktop <trentonford@gmail.com>
2021-03-30 13:43:50 -0400

    attempted fix for disambiguation error (doesn't work)

--- src/downloader.py
+++ src/downloader.py
@@ -1,12 +1,12 @@
 import os
 from argparse import ArgumentParser as ArgP
-from typing import Dict
+from typing import Dict, List
 from urllib.parse import urlparse
 
 import requests
 from mediawiki import MediaWiki, MediaWikiPage
 
-from src.utilities import verify_dir
+from src.utilities import verify_dir, verify_file
 
 
 class Person:
@@ -22,21 +22,29 @@ class Person:
 
 
 def retrieve_images(page_names: Dict, wikiObj, output_location='/home/tford5/faces_datasets/wikipedia/'):
+    skipped_downloads = 0
     for key, person in page_names.items():
         folder_name = key.lower().replace(' ', '_')
         output_path = f'{output_location}/{folder_name}/'
         verify_dir(output_path)
+
         for idx, link in enumerate(person.image_links):
-            r = requests.get(link)
+            # get filename
             parsed = urlparse(link)
             filename = os.path.basename(parsed.path)
             filename = filename.split('.')[0]
+            # check if file already exists in the location
+            if verify_file(output_path + str(filename)):
+                skipped_downloads += 1
+                break
+            r = requests.get(link)
             # the output images may not actually be jpgs
             output_filename = os.path.abspath(f'{output_path}/{filename}.jpg')
             with open(output_filename, 'wb') as f:
                 f.write(r.content)
             person.image_locations.append(output_location)
             break
+    print(f'Skipped Downloads: {skipped_downloads}')
     return page_names
 
 
@@ -51,7 +59,7 @@ if __name__ == '__main__':
 
     print(args.output_location)
     print(args.categories)
-    exit()
+    # exit()
     keys = args.categories
     wikipedia = MediaWiki()
     categories = wikipedia.categorytree(keys, depth=500)
@@ -67,24 +75,26 @@ if __name__ == '__main__':
             for p in pages:
                 if not p.startswith('Template:'):
                     if not p.startswith('Wikipedia:'):
-                        page: MediaWikiPage = wikipedia.page(title=p)
-                        person = Person(p)
-                        # print([' stubs' in cat for cat in page.categories], page.categories)
-                        if page.categories and not any([' stubs' in cat for cat in page.categories]):
-                            image_links = page.images
-                            for link in image_links:
-                                parsed = urlparse(link)
-                                filename = os.path.basename(parsed.path)
-                                # print(filename)
-                                # print(p)
-                                if ' ' in p:
-                                    if p.split(' ')[0] in filename or p.split(' ')[1] in filename:
-                                        print(page.url)
-                                        if p not in people_pages:
-                                            people_pages[p] = person
-                                        else:
-                                            person = people_pages[p]
-                                        person.add_image_links(link)
+                        candidate_pages: List[MediaWikiPage] = wikipedia.allpages(query=p)
+                        candidate_pages = [x for x in candidate_pages if r in x.categories]
+                        for page in candidate_pages:
+                            person = Person(page.title)
+                            # print([' stubs' in cat for cat in page.categories], page.categories)
+                            if page.categories and not any([' stubs' in cat for cat in page.categories]):
+                                image_links = page.images
+                                for link in image_links:
+                                    parsed = urlparse(link)
+                                    filename = os.path.basename(parsed.path)
+                                    # print(filename)
+                                    # print(p)
+                                    if ' ' in p:
+                                        if p.split(' ')[0] in filename or p.split(' ')[1] in filename:
+                                            print(page.url)
+                                            if p not in people_pages:
+                                                people_pages[page.title] = person
+                                            else:
+                                                person = people_pages[page.title]
+                                            person.add_image_links(link)
 
     print(people_pages)
     retrieve_images(people_pages, wikipedia, output_location=args.output_location)
