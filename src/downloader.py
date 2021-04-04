0b03ffe77d6bf7b47b671ee8726f8dbdb110ba73
Trenton W. Ford - Desktop <trentonford@gmail.com>
2021-04-04 19:27:58 -0400

    Trying some things to speed stuff up.

--- src/downloader.py
+++ src/downloader.py
@@ -1,17 +1,19 @@
 import os
+import pickle
 from argparse import ArgumentParser as ArgP
 from typing import Dict, Set
 from urllib.parse import urlparse
 
 import requests
 from mediawiki import MediaWiki, MediaWikiPage
+from tqdm import tqdm
 
 from src.utilities import Person, verify_dir, verify_file
 
 
 def retrieve_images(page_names: Dict, wikiObj, output_location='/data/tford5/faces_datasets/wikipedia/'):
     skipped_downloads = 0
-    for key, person in page_names.items():
+    for key, person in tqdm(page_names.items(), desc='Processing Image Downloads'):
         folder_name = key.lower().replace(' ', '_')
         output_path = f'{output_location}/{folder_name}/'
         verify_dir(output_path)
@@ -46,24 +48,31 @@ if __name__ == '__main__':
 
     print(args.output_location)
     print(args.categories)
-    # exit()
-    initial_categories = args.categories
-    wikipedia = MediaWiki()
-    categories = wikipedia.categorytree(initial_categories, depth=50)
+    initial_categories = set(args.categories)
+    wikipedia = MediaWiki(rate_limit=False)
+    print('Collecting Initial Categories...')
+    categories = wikipedia.categorytree(list(initial_categories), depth=1)
+    print(f'Initial Categories Collected: {len(categories)}')
     people_pages = {}
 
 
     # yes it's recursive
-    def get_pages(category, depth=0) -> Set:
-        pgs: Set = set()
-        scs: Set = set()
-        for cat in category:
-            data = wikipedia.categorymembers(category=cat, results=1000, subcategories=True)
+    def get_pages(category, seen_categories=set(), depth=0, max_depth=50) -> Set:
+        pbar.update(1)
+        pgs: Set = set()  # pages collected
+        scs: Set = set()  # categories collected
+        unseen_categories = category.difference(seen_categories)
+        for cat in unseen_categories:
+            data = wikipedia.categorymembers(category=cat, results=100, subcategories=True)
             pgs.update(data[0])
             scs.update(data[1])
-        if depth >= 50 or len(scs) == 0:
+            seen_categories.add(cat)
+        if depth >= max_depth or len(scs) == 0:
+            pbar2.update(1)
             return pgs
-        pgs.update(get_pages(scs, depth=depth + 1))
+
+        pgs.update(get_pages(scs, seen_categories=seen_categories, depth=depth + 1, max_depth=max_depth))
+        pbar2.update(1)
         return pgs
 
         # for k in initial_categories:
@@ -72,13 +81,30 @@ if __name__ == '__main__':
         # print(r)
 
 
-    pages = get_pages(initial_categories)
+    print('Getting Extended Categories...')
+    max_depth = 50
+    cache_filename = f'/data/tford5/faces_datasets/wikipedia/cached_{len(initial_categories)}.pkl'
+    if not verify_file(cache_filename):
+        pbar = tqdm(total=max_depth, desc="Descent")
+        pbar2 = tqdm(total=max_depth, desc="Ascent")
+        pages = get_pages(initial_categories, max_depth=max_depth)
+        with open(cache_filename, 'wb') as f:
+            pickle.dump(pages, file=f)
+    else:
+        with open(cache_filename, 'rb') as f:
+            pages = pickle.load(f)
+
+    print(f'Number of Pages Collected: {len(pages)}')
     nonperson_pages = 0
-    for p in pages:
+    dis_pages = []
+    for p in tqdm(pages, desc="Processing pages"):
         nonperson_pages += 1
         if not p.startswith('Template:') and not p.startswith('Wikipedia:'):
             # attempt to pull the page and handle disambiguation errors are they appear
-            page: MediaWikiPage = wikipedia.page(title=p, auto_suggest=False)
+            try:
+                page: MediaWikiPage = wikipedia.page(title=p, auto_suggest=False)
+            except Exception as e:
+                dis_pages.append(e)
             if any(['people' in cat for cat in page.categories]) or 'born' in page.summary:
                 nonperson_pages -= 1
                 if page.categories and not any([' stubs' in cat for cat in page.categories]):
@@ -94,4 +120,5 @@ if __name__ == '__main__':
     # for x in people_pages.items():
     #     print(x)
     print("Pages ignored: ", nonperson_pages)
+    print(f"Dis Pages: {len(dis_pages)}")
     retrieve_images(people_pages, wikipedia, output_location=args.output_location)
